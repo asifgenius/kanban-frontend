2eb0c6c560f18d222daf99de438ce0ff9e3a725b
asifgenius <asif15-11288@diu.edu.bd>
2024-11-08 14:59:24 +0600

    React initialize

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BoardView from './pages/BoardView/BoardView';
+
+function App() {
+  return (
+    <div>
+      <BoardView />
+    </div>
+  );
+}
+
+export default App;
